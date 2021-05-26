99f98ba1ba7eafb922edf33e1634cbe724acd52b
vincent <1131182508@qq.com>
2021-05-26 11:17:05 +0800

    类

--- 01-study/src/index.ts
+++ 01-study/src/index.ts
@@ -12,6 +12,7 @@ x = ['hello', 10]; // OK
 // x.push(2);
 // console.log(x[2]);
 
+// 接口（interface）
 interface Say {
   (words: string): string
 }
@@ -22,3 +23,19 @@ interface User {
   say: Say
 }
 const getUserName = (user: User) => user.name
+
+// 类
+abstract class Animal {
+  abstract makeSound(): void;
+  move(): void {
+    console.log('roaming the earch...');
+  }
+}
+class Cat extends Animal {
+  makeSound() {
+    console.log('miao miao')
+  }
+}
+const cat = new Cat()
+cat.makeSound() // miao miao
+cat.move() // roaming the earch...
